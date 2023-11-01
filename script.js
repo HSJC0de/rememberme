47ddbd94d8796d8044f6a0309539b6b44579b2c4
Joshua D <email@email.com>
2023-10-31 17:19:35 -0700

    created html, js, css files, created image folder with placeholder img, created layout

---
+++ script.js
@@ -0,0 +1,11 @@
+const addItem1 = document.getElementById("addItem1");
+const addItem2 = document.getElementById("addItem2");
+
+addItem1.addEventListener("click", function(){
+  console.log('addItem1')
+})
+
+addItem2.addEventListener("click", function(){
+  console.log('addItem2')
+})
+
