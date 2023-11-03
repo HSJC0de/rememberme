8058acd1c967fb3883c17ffcc5f46b6ff29a54be
HSJC0de <delacruz.joshuaprsnl@gmail.com>
2023-11-03 00:25:25 -0700

    created js to add a preview item to the screen before the addItemPreview  element

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const formPopUpSubmitButton = document.getElementById('formPopUpSubmitButton');
 const previewContainer = document.getElementById('previewContainer')
 const formPopUpBookTitle = document.getElementById('BookTitle');
 const formPopUpBookAuthor = document.getElementById('BookAuthor');
+const addItemPreview = document.getElementById('addItemPreview');
 
 function displayFormPopUp(){
   formPopUp.style.display = 'block';
@@ -26,18 +27,20 @@ formPopUp.addEventListener('submit', function(event){
 function addPreview(){
   let bookTitle = formPopUpBookTitle.value;
   let author = formPopUpBookAuthor.value;
+  let newElement = document.createElement('div');
+  newElement.className = "col d-flex justify-content-center mb-5 mt-5";
+  newElement.innerHTML = 
+  `
+  
+  <div class="card" style="width: 20rem; height: 25rem;">
+    <img src="images/image placeholder.svg" alt="" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">${bookTitle}</h5>
+      <h6 class="card-subtitle text-muted">${author}</h6>
+      <button class="btn btn-primary mt-4">Open</button>
+      <button class="btn btn-secondary mt-4">Share</button>
+    </div>
+  </div>`;
 
-  previewContainer.innerHTML += `
-  <div class="col d-flex justify-content-center mb-5 mt-5">
-            <div class="card" style="width: 20rem; height: 25rem;">
-              <img src="images/image placeholder.svg" alt="" class="card-img-top">
-              <div class="card-body">
-                <h5 class="card-title">${bookTitle}</h5>
-                <h6 class="card-subtitle text-muted">${author}</h6>
-                <button class="btn btn-primary mt-4">Open</button>
-                <button class="btn btn-secondary mt-4">Share</button>
-              </div>
-            </div>
-          </div>
-  `;
+addItemPreview.parentNode.insertBefore(newElement, addItemPreview);
 }
